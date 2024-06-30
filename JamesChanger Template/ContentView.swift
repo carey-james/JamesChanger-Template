c7ef930bc340994eb5236f5ab1d1b4649bdc12c2
James Carey <57900176+carey-james@users.noreply.github.com>
2024-06-30 17:05:59 -0400

    Initial Commit

---
+++ JamesChanger Template/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JamesChanger Template
+//
+//  Created by James Carey on 6/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JamesChanger Template/JamesChanger_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamesChanger_TemplateApp.swift
+//  JamesChanger Template
+//
+//  Created by James Carey on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct JamesChanger_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
